5374dac60bcdee1013df0371cc8625aa6c7f2716
U-BJSS\Des.McCarter <Des.McCarter@LPT2177.bjss.co.uk>
2016-06-15 14:23:42 +0100

    added runbash

---
+++ runbash.sh
@@ -0,0 +1,34 @@
+args="${*}"
+
+. ${DEVELOPMENT}/utils/utils.sh
+. ${DEVELOPMENT}/cdexample/createvm.sh
+
+
+function runbash(){
+
+	script="${1}" 
+	vm="${2}"
+	vm_admin_user="${3}"
+
+	if [ -z "${vm_admin_user}" ]
+	then
+		error "runbash: no vm admin user for thie vm"
+		return 1
+	fi
+
+	if [ -z "${script}" ]
+	then
+		error "runbash: no script given"
+		return 1
+	fi
+
+	if [ -z "${vm}" ]
+	then
+		error "runbash: no vm given"
+		return 1
+	fi
+
+	ip="`getProperty vm.instance.${vm} ${PROPERTIES}`"
+
+	ssh "${vm_admin_user}@${ip}" "${script}"
+}
